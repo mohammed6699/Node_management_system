e30f249b5c09bee3502d62dfcb635ff12e1c2967
Ahmad Tarek <96955833+Abotareq@users.noreply.github.com>
2025-06-04 08:54:30 +0300

    cron update

--- reminders/reminder.js
+++ reminders/reminder.js
@@ -1,6 +1,19 @@
 // fiter un finished , pendding tasks
-import taskModel from '../models/taskModel.js'
-import { schedule } from 'node-cron'
-const remiderTasks = schedule("0 9 * * *", async() => {
-    
-})
+import taskModel from "../models/taskModel.js";
+import { schedule } from "node-cron";
+import taskModel from "../models/taskModel.js";
+import userModels from "../models/userModels.js";
+const sendMail = () => {
+  try {
+    taskModel.find({ status: "pendding" }).then((data) => {
+      data.forEach((task) => {
+        //nodemailer for task User.id
+        //user.email
+        console.log(task);
+      });
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+const remiderTasks = schedule("* * * * * *",sendMail);
